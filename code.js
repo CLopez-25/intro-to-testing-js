d070da94819b99ad682a000b66063dc9f5d4fac9
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-08-24 09:27:19 -0500

    up to exercise 11 completed and tested

--- code.js
+++ code.js
@@ -2,3 +2,33 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(input){
+    if(typeof input === "string"){
+        return "Hello, " + input + "!";
+    } else if(typeof input === "boolean"){
+        return "Hello, World!";
+    } else {
+        return "Hello, World!";
+    }
+}
+
+function isFive(input){
+    if(input === 5){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isEven(input){
+    if(parseFloat(input)% 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isVowel(input){
+
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,70 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
+    it('should return the string "Hello, Alex!" when executed', function (){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat! when executed', function (){
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, Jane! when executed', function (){
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, World!" when executed', function (){
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when the function is true', function (){
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when the function is false', function (){
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+});
+
+describe("isFive", function (){
+    it('should be a defined function', function (){
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return true if input is 5', function (){
+        expect(isFive(5)).toBe(true);
+    });
+    it('should return a boolean when called', function (){
+        expect(typeof isFive()).toBe("boolean");
+    });
+});
+
+describe("isEven",  function (){
+    it('should be a defined function', function (){
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean when called', function (){
+        expect(typeof isEven()).toBe("boolean");
+    });
+    it('should return true when the input is even', function (){
+        expect(isEven(0)).toBe(true);
+    });
+});
+
+describe("isVowel", function (){
+    it('should be a defined function', function (){
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should return a boolean when called', function (){
+        expect(typeof isVowel()).toBe("boolean");
+    });
+    it('should return true when the input is a vowel', function (){
+        expect().toBe(true);
+    })
+})
+
